98a646be3eb296c5db3bad0d9b2a7e3f5203130a
Marcio <marcio@zimbres.com>
2025-03-30 23:40:25 -0300

    Add SQL for manual DB migration

---
+++ SSLTrack/Migrations/AddSilence.sql
@@ -0,0 +1,9 @@
+﻿BEGIN TRANSACTION;
+
+ALTER TABLE "Domains" ADD "Silenced" INTEGER NOT NULL DEFAULT 0;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250329200311_AddSilence', '8.0.14');
+
+COMMIT;
+
